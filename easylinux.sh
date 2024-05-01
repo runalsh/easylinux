c7b3e70b90d9f5ee6b90100d128ae32e330a98b6
runalsh <21105673+runalsh@users.noreply.github.com>
2024-05-01 13:33:59 +0300

    Update easylinux.sh

--- easylinux.sh
+++ easylinux.sh
@@ -17,7 +17,7 @@ source configself.sh
 apt-get update
 apt-get install -y --no-install-recommends --no-install-suggests \
   kmod debian-archive-keyring tzdata software-properties-common lsb-release apt-transport-https apt-utils sudo coreutils \
-  ncdu wget net-tools iputils-ping curl ca-certificates iproute2 \
+  ncdu wget net-tools iputils-ping curl ca-certificates iproute2 dnsutils \
   nano procps tree telnet tmux bash-completion grep gawk mc patch apache2-utils nmon jq tar python3 python3-pip zip unzip git lzma gpg
 #tig iptables-persistent
 timedatectl set-timezone Europe/Moscow
